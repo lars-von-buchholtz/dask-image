e7fbee21d9a639486cf740bce5dbe94430286e04
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-11-20 19:11:44 -0500

    add __init__ to ndfeature

---
+++ dask_image/ndfeature/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Lars von Buchholtz"""
+__email__ = "lars.von.buchholtz@gmail.com"
+
+
+from ._feature import (
+    blob_log
+)
+
+
+blob_log.__module__ = __name__
+#peak_local_max.__module__ = __name__
