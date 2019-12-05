6188c6d6239460bf4e7d70298278c8709db9aa6f
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-12-05 09:38:20 -0500

    rechunk sigma dimension of image stack

--- dask_image/ndfeature/_feature.py
+++ dask_image/ndfeature/_feature.py
@@ -1,14 +1,19 @@
 import numpy as np
-from scipy.ndimage import gaussian_filter, gaussian_laplace
-#import math
+from scipy.ndimage import gaussian_filter
+import math
 #from math import sqrt, log
 #from scipy import spatial
+#from math import sqrt
+import dask.array as da
+from scipy import spatial, ndimage as ndi
 from skimage import img_as_float
 from skimage.feature.peak import peak_local_max as ski_peak_local_max
-#from ._hessian_det_appx import _hessian_matrix_det
+from skimage.feature._hessian_det_appx import _hessian_matrix_det
 #from ..transform import integral_image
 #from .._shared.utils import check_nD
 from ..ndmeasure import labeled_comprehension
+from ..ndfilters._gaussian import _get_sigmas, _get_border, gaussian_laplace
+from ..ndfilters import _utils
 
 
 
@@ -94,7 +99,7 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
     The radius of each blob is approximately :math:`\sqrt{2}\sigma` for
     a 2-D image and :math:`\sqrt{3}\sigma` for a 3-D image.
     """
-    image = img_as_float(image)
+    #image = img_as_float(image) #check this out
 
     # if both min and max sigma are scalar, function returns only one sigma
     scalar_sigma = (
@@ -128,9 +133,12 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
     gl_images = [-gaussian_laplace(image, s) * np.mean(s) ** 2
                  for s in sigma_list]
 
-    image_cube = np.stack(gl_images, axis=-1)
+    image_cube = da.stack(gl_images, axis=-1)
+    chunk_shape = image_cube.chunks
+    new_shape = chunk_shape[:-1] +((sum(chunk_shape[-1]),),)
+    image_cube = image_cube.rechunk(chunks=new_shape)
 
-    local_maxima = peak_local_max(image_cube, threshold_abs=threshold,
+    local_maxima = _peak_local_max(image_cube, threshold_abs=threshold,
                                   footprint=np.ones((3,) * (image.ndim + 1)),
                                   threshold_rel=0.0,
                                   exclude_border=exclude_border)
@@ -158,117 +166,5 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
     return _prune_blobs(lm, overlap, sigma_dim=sigma_dim)
 
 
-def _peak_local_max(image, min_distance=1, threshold_abs=None,
-                   threshold_rel=None, exclude_border=True, indices=True,
-                   num_peaks=np.inf, footprint=None, labels=None,
-                   num_peaks_per_label=np.inf):
-    """Find peaks in an image as coordinate list or boolean mask.
-    Peaks are the local maxima in a region of `2 * min_distance + 1`
-    (i.e. peaks are separated by at least `min_distance`).
-    If there are multiple local maxima with identical pixel intensities
-    inside the region defined with `min_distance`,
-    the coordinates of all such pixels are returned.
-    If both `threshold_abs` and `threshold_rel` are provided, the maximum
-    of the two is chosen as the minimum intensity threshold of peaks.
-    Parameters
-    ----------
-    image : ndarray
-        Input image.
-    min_distance : int, optional
-        Minimum number of pixels separating peaks in a region of `2 *
-        min_distance + 1` (i.e. peaks are separated by at least
-        `min_distance`).
-        To find the maximum number of peaks, use `min_distance=1`.
-    threshold_abs : float, optional
-        Minimum intensity of peaks. By default, the absolute threshold is
-        the minimum intensity of the image.
-    threshold_rel : float, optional
-        Minimum intensity of peaks, calculated as `max(image) * threshold_rel`.
-    exclude_border : int or bool, optional
-        If nonzero int, `exclude_border` excludes peaks from
-        within `exclude_border`-pixels of the border of the image.
-        If True, takes the `min_distance` parameter as value.
-        If zero or False, peaks are identified regardless of their
-        distance from the border.
-    indices : bool, optional
-        If True, the output will be an array representing peak
-        coordinates.  If False, the output will be a boolean array shaped as
-        `image.shape` with peaks present at True elements.
-    num_peaks : int, optional
-        Maximum number of peaks. When the number of peaks exceeds `num_peaks`,
-        return `num_peaks` peaks based on highest peak intensity.
-    footprint : ndarray of bools, optional
-        If provided, `footprint == 1` represents the local region within which
-        to search for peaks at every point in `image`.  Overrides
-        `min_distance` (also for `exclude_border`).
-    labels : ndarray of ints, optional
-        If provided, each unique region `labels == value` represents a unique
-        region to search for peaks. Zero is reserved for background.
-    num_peaks_per_label : int, optional
-        Maximum number of peaks for each label.
-    Returns
-    -------
-    output : ndarray or ndarray of bools
-        * If `indices = True`  : (row, column, ...) coordinates of peaks.
-        * If `indices = False` : Boolean array shaped like `image`, with peaks
-          represented by True values.
-    Notes
-    -----
-    The peak local maximum function returns the coordinates of local peaks
-    (maxima) in an image. A maximum filter is used for finding local maxima.
-    This operation dilates the original image. After comparison of the dilated
-    and original image, this function returns the coordinates or a mask of the
-    peaks where the dilated image equals the original image.
-    See also
-    --------
-    skimage.feature.corner_peaks
-    Examples
-    --------
-    >>> img1 = np.zeros((7, 7))
-    >>> img1[3, 4] = 1
-    >>> img1[3, 2] = 1.5
-    >>> img1
-    array([[0. , 0. , 0. , 0. , 0. , 0. , 0. ],
-           [0. , 0. , 0. , 0. , 0. , 0. , 0. ],
-           [0. , 0. , 0. , 0. , 0. , 0. , 0. ],
-           [0. , 0. , 1.5, 0. , 1. , 0. , 0. ],
-           [0. , 0. , 0. , 0. , 0. , 0. , 0. ],
-           [0. , 0. , 0. , 0. , 0. , 0. , 0. ],
-           [0. , 0. , 0. , 0. , 0. , 0. , 0. ]])
-    >>> peak_local_max(img1, min_distance=1)
-    array([[3, 4],
-           [3, 2]])
-    >>> peak_local_max(img1, min_distance=2)
-    array([[3, 2]])
-    >>> img2 = np.zeros((20, 20, 20))
-    >>> img2[10, 10, 10] = 1
-    >>> peak_local_max(img2, exclude_border=0)
-    array([[10, 10, 10]])
-    """
 
-    # calculate depth and boundaries based on min_distance and/or footprint
-    if not (min_distance or footprint):
-        raise ValueError('Either min_distance or footprint must be specified')
-
-    depth = 2 * min_distance + 1
-    if footprint:
-        depth = footprint.shape
-
-    # map_overlap plm without border exclude, labels, indices=False
-    result = image.map_overlap(
-        ski_peak_local_max,
-        depth=depth,
-        min_distance=min_distance, threshold_abs=threshold_abs,
-        threshold_rel=threshold_rel, exclude_border=False, indices=False,
-        num_peaks=np.inf, footprint=footprint, labels=None,
-        num_peaks_per_label=np.inf
-    )
-
-    # if exclude_borders filter out points near borders
-    if type(exclude_border) == bool:
-        exclude_border = min_distance if exclude_border else 0
-
-    # if max number of indices check for that
-
-    #
 
