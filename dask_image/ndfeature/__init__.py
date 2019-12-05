846bda1d0fc9fdb1b9599c508d351a7290dde440
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-12-05 16:30:50 -0500

    add peak_local-max to init

--- dask_image/ndfeature/__init__.py
+++ dask_image/ndfeature/__init__.py
@@ -5,9 +5,10 @@ __email__ = "lars.von.buchholtz@gmail.com"
 
 
 from ._feature import (
-    blob_log
+    blob_log,
+    peak_local_max
 )
 
 
 blob_log.__module__ = __name__
-#peak_local_max.__module__ = __name__
+peak_local_max.__module__ = __name__
