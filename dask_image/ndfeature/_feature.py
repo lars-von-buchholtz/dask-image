d4b7c245e9e87031777e5308a185b49fe3a2653a
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-12-05 16:42:59 -0500

    edit docstring of blob_log

--- dask_image/ndfeature/_feature.py
+++ dask_image/ndfeature/_feature.py
@@ -1,19 +1,14 @@
 import numpy as np
-from scipy.ndimage import gaussian_filter
 import math
-#from math import sqrt, log
-#from scipy import spatial
-#from math import sqrt
+
+
 import dask.array as da
-from scipy import spatial, ndimage as ndi
-from skimage import img_as_float
+
 from skimage.feature.peak import peak_local_max as ski_peak_local_max
 from skimage.feature._hessian_det_appx import _hessian_matrix_det
-#from ..transform import integral_image
+
 #from .._shared.utils import check_nD
-from ..ndmeasure import labeled_comprehension
 from ..ndfilters._gaussian import _get_sigmas, _get_border, gaussian_laplace
-from ..ndfilters import _utils
 from ._skimage_utils import _exclude_border, _prune_blobs
 from functools import wraps
 
@@ -250,7 +245,7 @@ def blob_common(blob_func):
 
 
 
-
+@blob_common
 def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
              overlap=.5, log_scale=False, *, exclude_border=False):
     r"""Finds blobs in the given grayscale image.
@@ -261,8 +256,8 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
     deviation of the Gaussian kernel that detected the blob.
     Parameters
     ----------
-    image : 2D or 3D ndarray
-        Input grayscale image, blobs are assumed to be light on dark
+    image : dask array
+        Input grayscale image as n-dimensional dask array, blobs are assumed to be light on dark
         background (white on black).
     min_sigma : scalar or sequence of scalars, optional
         the minimum standard deviation for Gaussian kernel. Keep this low to
@@ -285,7 +280,7 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
         A value between 0 and 1. If the area of two blobs overlaps by a
         fraction greater than `threshold`, the smaller blob is eliminated.
     log_scale : bool, optional
-        If set intermediate values of standard deviations are interpolated
+        If set, intermediate values of standard deviations are interpolated
         using a logarithmic scale to the base `10`. If not, linear
         interpolation is used.
     exclude_border : int or bool, optional
@@ -293,25 +288,23 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
         within `exclude_border`-pixels of the border of the image.
     Returns
     -------
-    A : (n, image.ndim + sigma) ndarray
-        A 2d array with each row representing 2 coordinate values for a 2D
-        image, and 3 coordinate values for a 3D image, plus the sigma(s) used.
-        When a single sigma is passed, outputs are:
-        ``(r, c, sigma)`` or ``(p, r, c, sigma)`` where ``(r, c)`` or
-        ``(p, r, c)`` are coordinates of the blob and ``sigma`` is the standard
-        deviation of the Gaussian kernel which detected the blob. When an
-        anisotropic gaussian is used (sigmas per dimension), the detected sigma
-        is returned for each dimension.
+    A : (m, image.ndim + sigma) ndarray
+        A 2d array with each row representing m coordinate values for a m-dimensional
+        image plus the sigma(s) used.
+        When a single sigma is passed both for min_sigma and max_sigma, the last column is the standard
+        deviation of the gaussian that detected the blob resulting in an m * (n + 1) array.
+         When an anisotropic gaussian is used (sigmas per dimension), the detected
+        sigma is returned for each dimension resulting in an m * 2n array.
     References
     ----------
     .. [1] https://en.wikipedia.org/wiki/Blob_detection#The_Laplacian_of_Gaussian
     Examples
     --------
     >>> from skimage import data, exposure
-    >>> from dask_image import
+    >>> from dask_image.ndfeature import blob_log
     >>> img = data.coins()
     >>> img = exposure.equalize_hist(img)  # improves detection
-    >>> feature.blob_log(img, threshold = .3)
+    >>> blob_log(img, threshold = .3)
     array([[266.        , 115.        ,  11.88888889],
            [263.        , 302.        ,  17.33333333],
            [263.        , 244.        ,  17.33333333],
@@ -334,6 +327,7 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
     The radius of each blob is approximately :math:`\sqrt{2}\sigma` for
     a 2-D image and :math:`\sqrt{3}\sigma` for a 3-D image.
     """
+
     if log_scale:
         # for anisotropic data, we use the "highest resolution/variance" axis
         standard_axis = np.argmax(min_sigma)
