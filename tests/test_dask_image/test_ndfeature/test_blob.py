9be583e8467f624a98db5febae108966f4227954
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-12-10 13:05:33 -0500

    start implementing test_blob

--- tests/test_dask_image/test_ndfeature/test_blob.py
+++ tests/test_dask_image/test_ndfeature/test_blob.py
@@ -4,30 +4,96 @@
 import pytest
 
 import numpy as np
-import skimage.feature as ski_feat
+
 from scipy.ndimage.filters import gaussian_filter
 
 import dask
 import dask.array as da
 import dask.array.utils as dau
 
+import skimage.feature as ski_feat
 import dask_image.ndfeature as da_feat
 
 assert dask
 
-def generate_blobimage(shape,blobs):
-    """function to generate blob images"""
-    img = np.zeros(shape,dtype=np.float)
+
+def generate_blobimage(shape, blobs):
+    """function to generate blob images
+
+
+    :param shape:shape of image to generate
+    :param blobs: array with blob coordinates and sigma as rows"""
+
+    img = np.zeros(shape, dtype=np.float)
+    if blobs is None:
+        return img
     for blob in blobs:
-        tmp_img = np.zeros(shape,dtype=np.float)
+        tmp_img = np.zeros(shape, dtype=np.float)
         tmp_img[blob[:-1]] = 1.0
-        gaussian_filter(tmp_img,blob[-1],output=tmp_img)
+        gaussian_filter(tmp_img, blob[-1], output=tmp_img)
         img += tmp_img
     return img
 
-#@pytest.mark.parametrize("da_func,ski_func",[(da_blob_log,ski_blob_log),
-#                                             (da_blob_dog,ski_blob_dog)])
-# @pytest.mark.parametrize()
+
+@pytest.mark.parametrize(
+    "da_func,ski_func",
+    [
+        (da_feat.blob_log, ski_feat.blob_log),
+        (da_feat.blob_dog, ski_feat.blob_dog),
+        (da_feat.blob_doh, ski_feat.blob_doh),
+    ],
+)
+@pytest.mark.parametrize(
+    "shape, blobs",
+    [
+        ((100, 200), None),
+        ((100, 200), np.array([[50, 50, 5]])),
+        ((100, 200), np.array([[50, 50, 5], [49, 99, 10]])),
+        ((100, 200), np.array([[50, 50, 5], [49, 99, 10], [52, 102, 10]])),
+    ],
+)
+@pytest.mark.parametrize(
+    "min_sigma, max_sigma, num_sigma, sigma_ratio",
+    [
+        (1, None),
+        (5, None),
+        (1, np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])),
+        (1, np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])),
+    ],
+)
+@pytest.mark.parametrize("threshold", [0.001, 0.1, 0.9])
+@pytest.mark.parametrize("overlap", [0.1, 0.5, 0.9])
+@pytest.mark.parametrize("log_scale", [True, False])
+def test_peak_local_max_2d(
+    shape, blobs, min_distance, footprint, threshold_abs, num_peaks
+):
+    a = make_img(shape, points)
+    chunks = [e // 2 for e in shape]
+    d = da.from_array(a, chunks=chunks)
+    ski_r = ski_plm(
+        a,
+        min_distance=min_distance,
+        threshold_abs=threshold_abs,
+        footprint=footprint,
+        num_peaks=num_peaks,
+        exclude_border=False,
+    )
+    # sort the arrays
+    ski_r = ski_r[ski_r[:, 1].argsort(kind="mergesort")]
+    ski_r = ski_r[ski_r[:, 0].argsort(kind="mergesort")]
+    da_r = da_plm(
+        d,
+        min_distance=min_distance,
+        threshold=threshold_abs,
+        footprint=footprint,
+        num_peaks=num_peaks,
+        exclude_border=False,
+    )
+    da_r = da_r[da_r[:, 1].argsort(kind="mergesort")]
+    da_r = da_r[da_r[:, 0].argsort(kind="mergesort")]
+    dau.assert_eq(ski_r, da_r)
+
+
 # def test_2d():
 
 # test 0,1,2,3 blobs are detected with single sigma
@@ -47,4 +113,3 @@ def generate_blobimage(shape,blobs):
 # test exclude border eliminates blobs
 
 # test 3D and 4D
-
