b3c660f30f6afc51390a25acf79eda58f5e4e8a8
Lars von Buchholtz <lars.von.buchholtz@gmail.com>
2019-12-10 13:04:29 -0500

    add blob_dog and blob_doh to init

--- dask_image/ndfeature/__init__.py
+++ dask_image/ndfeature/__init__.py
@@ -6,9 +6,13 @@ __email__ = "lars.von.buchholtz@gmail.com"
 
 from ._feature import (
     blob_log,
+    blob_dog,
+    blob_doh,
     peak_local_max
 )
 
 
 blob_log.__module__ = __name__
+blob_dog.__module__ = __name__
+blob_doh.__module__ = __name__
 peak_local_max.__module__ = __name__
